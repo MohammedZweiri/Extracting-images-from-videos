6b440f2f71a6b99ff8b9d438fb5635c91b65611d
MohammedZweiri <53436926+MohammedZweiri@users.noreply.github.com>
2021-02-02 19:24:16 +0000

    Update Input_Images.m

--- Input_Images.m
+++ Input_Images.m
@@ -1,15 +1,15 @@
 close all
 clc
+# Export your video to MATLAB before starting
 
-
- filename = Space_debris_1;
+ filename = Your video name;
 
 [rows, cols, channels, samples] = size(filename);
 figure;
 for i = 1:samples
     str = sprintf('Sample: %d ', i);  
     imshow(filename(:,:,:,i))
-    imwrite(filename(:,:,:,i),['Space_debris_1_' num2str(i) '.jpg'] ,'jpg')
+    imwrite(filename(:,:,:,i),['New file name' num2str(i) '.jpg'] ,'jpg')    #This will save your generated images to whatever name you give
     title(str);
     pause(0.1);
 end
